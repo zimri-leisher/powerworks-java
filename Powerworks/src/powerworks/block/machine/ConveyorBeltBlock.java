9e6163bec520bd39e163c06d31ef40fb20ff0161
Zimri <cyborghhbk@gmail.com>
2017-04-28 15:23:04 -0400

    GUI fixes

--- Powerworks/src/powerworks/block/machine/ConveyorBeltBlock.java
+++ Powerworks/src/powerworks/block/machine/ConveyorBeltBlock.java
@@ -1,13 +1,35 @@
 package powerworks.block.machine;
 
-public class ConveyorBeltBlock extends MachineBlock {
+import powerworks.event.EventHandler;
+import powerworks.event.EventListener;
+import powerworks.event.EventManager;
+import powerworks.event.PlaceBlockEvent;
+
+public class ConveyorBeltBlock extends MachineBlock implements EventListener{
 
     public static final int CONVEYOR_BELT_ACCELERATION = 1;
     
+    /**
+     * 0 = no curve, 1 = curve right, 2 = curve left. Relative to direction
+     */
+    public int curve = 0;
+    
     public ConveyorBeltBlock(MachineBlockType type, int xTile, int yTile) {
 	super(type, xTile, yTile);
+	EventManager.registerEventListener(this);
     }
 
+    @EventHandler
+    public void onPlaceBlockEvent(PlaceBlockEvent e) {
+	if(Math.abs(e.xTile - (xPixel >> 4)) <= 1 && Math.abs(e.yTile - (yPixel >> 4)) <= 1) {
+	    updateDirection();
+	}
+    }
+    
+    private void updateDirection() {
+	
+    }
+    
     @Override
     public void update() {
 	//for (Moving moving : Moving.movingEntities.getIntersecting(xPixel, yPixel, 16, 16)) {
---
+++ Powerworks/src/powerworks/graphics/screen/gui/LoadingScreenGUI.java
@@ -0,0 +1,9 @@
+package powerworks.graphics.screen.gui;
+
+
+public class LoadingScreenGUI extends GUI{
+
+    protected LoadingScreenGUI(int xPixel, int yPixel, int widthPixels, int heightPixels, String name, String background) {
+	super(xPixel, yPixel, widthPixels, heightPixels);
+    }
+}
